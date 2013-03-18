3612b50ecbc3a31c960b3b7795e6cddb3f425e02
Michal Potrzebicz <michal@elevendroids.com>
2013-03-18 23:06:54 +0100

    [delay] Cleanup

--- include/delay.h
+++ include/delay.h
@@ -1,7 +1,7 @@
 #ifndef DELAY_H
 #define DELAY_H
 
-extern inline void delay_us(unsigned int useconds);
-extern inline void delay_ms(unsigned int mseconds);
+void delay_us(unsigned int useconds);
+void delay_ms(unsigned int mseconds);
 
 #endif
