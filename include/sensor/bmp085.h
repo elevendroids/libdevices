fa7d1932cce48906c0bbf9be5b9a4d27bec2c3ac
Michal Potrzebicz <michal@elevendroids.com>
2013-05-29 00:30:43 +0200

    [sensor/bmp085] Fixed and optimized for MSP430

--- include/sensor/bmp085.h
+++ include/sensor/bmp085.h
@@ -6,13 +6,27 @@
 
 #define BMP085_ADDR 0x77
 
+#define BMP085_REG_AC1	0xAA
+#define BMP085_REG_AC2	0xAC
+#define BMP085_REG_AC3	0xAE
+#define BMP085_REG_AC4	0xB0
+#define BMP085_REG_AC5	0xB2
+#define BMP085_REG_AC6	0xB4
+#define BMP085_REG_B1	0xB6
+#define BMP085_REG_B2	0xB8
+#define BMP085_REG_MB	0xBA
+#define BMP085_REG_MC	0xBC
+#define BMP085_REG_MD	0xBE
+
+
+
 #define BMP085_SAMPLES_1 0
 #define BMP085_SAMPLES_2 1
 #define BMP085_SAMPLES_4 2
 #define BMP085_SAMPLES_8 3
 
 extern int Bmp085_Init(I2cDevice *device);
-extern int Bmp085_ReadPressure(I2cDevice *device, uint8_t oversampling);
+extern int32_t Bmp085_ReadPressure(I2cDevice *device, uint8_t oversampling);
 extern int Bmp085_ReadTemperature(I2cDevice *device);
 int Bmp085_ReadChipId(I2cDevice *device);
 int Bmp085_ReadChipVersion(I2cDevice *device);
--- sensor/bmp085.c
+++ sensor/bmp085.c
@@ -1,27 +1,50 @@
 #include "sensor/bmp085.h"
 #include "bus/i2c.h"
 #include "delay.h"
+#include "byteswap.h"
 
 #define get_short(c, i) (int16_t) ((c[i] << 8) + c[i + 1])
 #define get_ushort(c, i) (uint16_t) ((c[i] << 8) + c[i + 1])
 
-#define CALIBRATION_DATA_LEN 22
+typedef struct {
+	int16_t ac1, ac2, ac3;
+	int16_t b1, b2;
+	int16_t mb, mc, md;
+	uint16_t ac4, ac5, ac6;
+} CalibrationData1;
 
-typedef uint8_t CalibrationData[CALIBRATION_DATA_LEN];
+static CalibrationData1 cal_data;
 
 static const uint8_t wait_time_ms[] = { 5, 8, 14, 26 };
 
-static inline int Bmp085_ReadCalibrationData(I2cDevice *device, CalibrationData *data)
+static int16_t Bmp085_Read16(I2cDevice *device, uint8_t address)
 {
-	return I2c_ReadRegister(device, 0xAA, data, CALIBRATION_DATA_LEN);
+	uint16_t result;
+	I2c_ReadRegister(device, address, &result, sizeof(result));
+	return bswap_16(result);
 }
 
-static int Bmp085_ReadTemperatureRef(I2cDevice *device, CalibrationData *calibration, int32_t *value)
+int Bmp085_Init( I2cDevice *device)
+{
+	cal_data.ac1 = Bmp085_Read16(device, BMP085_REG_AC1);
+	cal_data.ac2 = Bmp085_Read16(device, BMP085_REG_AC2);
+	cal_data.ac3 = Bmp085_Read16(device, BMP085_REG_AC3);
+	cal_data.b1 = Bmp085_Read16(device, BMP085_REG_B1);
+	cal_data.b2 = Bmp085_Read16(device, BMP085_REG_B2);
+	cal_data.mb = Bmp085_Read16(device, BMP085_REG_MB);
+	cal_data.mc = Bmp085_Read16(device, BMP085_REG_MC);
+	cal_data.md = Bmp085_Read16(device, BMP085_REG_MD);
+	cal_data.ac4 = Bmp085_Read16(device, BMP085_REG_AC4);
+	cal_data.ac5 = Bmp085_Read16(device, BMP085_REG_AC5);
+	cal_data.ac6 = Bmp085_Read16(device, BMP085_REG_AC6);
+	return 0;
+}
+
+static int Bmp085_ReadTemperatureRef(I2cDevice *device, int32_t *value)
 {
 	int status;
 	uint8_t data[2];
-	uint16_t ut, ac5, ac6;
-	int16_t mc, md;
+	uint16_t ut;
 	int32_t x1, x2;
 	
 	// start temperature conversion
@@ -34,15 +57,11 @@ static int Bmp085_ReadTemperatureRef(I2cDevice *device, CalibrationData *calibra
 	if (status < 0) {
 		return status;
 	}
-	ut = (data[0] << 8) + data[1];
+	ut = ((uint16_t)data[0] << 8) + data[1];
 
-	ac5 = get_ushort((*calibration), 8);
-	ac6 = get_ushort((*calibration), 10);
-	mc = get_short((*calibration), 18);
-	md = get_short((*calibration), 20);
 	
-	x1 = ((ut - ac6) * ac5) >> 15;
-	x2 = (mc << 11) / (x1 + md);
+	x1 = (((uint32_t)ut - (uint32_t)cal_data.ac6) * (uint32_t)cal_data.ac5) >> 15;
+	x2 = ((int32_t)cal_data.mc << 11) / (x1 + cal_data.md);
 
 	*value = (x1 + x2);
 	return 0;
@@ -52,37 +71,24 @@ int Bmp085_ReadTemperature(I2cDevice *device)
 {
 	int status;
 	int32_t value;
-	CalibrationData calibration;
 
-	status = Bmp085_ReadCalibrationData(device, &calibration);
-	if (status < 0) {
-		return status;
-	}
-	
-	status = Bmp085_ReadTemperatureRef(device, &calibration, &value);
+	status = Bmp085_ReadTemperatureRef(device, &value);
 	if (status >= 0) {
-		return (value + 8) >> 4;
+		value = (value + 8) >> 4;
+		return value;
 	} else {
 		return status;
 	}
 }
 
-int Bmp085_ReadPressure(I2cDevice *device, uint8_t oversampling)
+int32_t Bmp085_ReadPressure(I2cDevice *device, uint8_t oversampling)
 {
 	int status;
 	uint8_t data[3];
-	CalibrationData calibration;
-	int16_t ac1, ac2, ac3, b1, b2;
-	uint16_t ac4;
 	int32_t b3, b5, b6, b6_2, x1, x2, x3, up, p;
 	uint32_t b4, b7;
 
-	status = Bmp085_ReadCalibrationData(device, &calibration);
-	if (status < 0) {
-		return status;
-	}
-	
-	status = Bmp085_ReadTemperatureRef(device, &calibration, &b5);
+	status = Bmp085_ReadTemperatureRef(device, &b5);
 	if (status < 0) {
 		return status;
 	}
@@ -100,30 +106,25 @@ int Bmp085_ReadPressure(I2cDevice *device, uint8_t oversampling)
 	
 	up = (((int32_t)data[0] << 16) + ((int32_t)data[1] << 8) + data[0]) >> (8 - oversampling);
 
-	ac1 = get_short(calibration, 0);
-	ac2 = get_short(calibration, 2);
-	ac3 = get_short(calibration, 4);
-	ac4 = get_ushort(calibration, 6);
-	b1 = get_short(calibration, 12);
-	b2 = get_short(calibration, 14);
-
 	b6 = b5 - 4000;
-	b6_2 = b6 * b6 >> 12;
-	x1 = (b2 * b6_2) >> 11;
-	x2 = ac2 * b6 >> 11; 
+	b6_2 = (b6 * b6) >> 12;
+	x1 = ((int32_t)cal_data.b2 * b6_2) >> 11;
+	x2 = ((int32_t)cal_data.ac2 * b6) >> 11; 
 	x3 = x1 + x2;
-	b3 = (((ac1 * 4 + x3) << oversampling) + 2) >> 2;
+	b3 = ((((int32_t)cal_data.ac1 * 4 + x3) << oversampling) + 2) >> 2;
 
-	x1 = ac3 * b6 >> 13;
-	x2 = (b1 * b6_2) >> 16;
+	x1 = ((int32_t)cal_data.ac3 * b6) >> 13;
+	x2 = ((int32_t)cal_data.b1 * b6_2) >> 16;
 	x3 = ((x1 + x2) + 2) >> 2;
-	b4 = (ac4 * (uint32_t)(x3 + 32768)) >> 15;
+	b4 = ((uint32_t)cal_data.ac4 * (uint32_t)(x3 + 32768)) >> 15;
 	b7 = ((uint32_t)up - b3) * (50000 >> oversampling);
+
 	if (b7 < 0x80000000) {
 		p = (b7 * 2) / b4;
 	} else {
 		p = (b7 / b4) * 2;
 	}
+
 	x1 = (p >> 8) * (p >> 8);
 	x1 = (x1 * 3038) >> 16;
 	x2 = (-7357 * p) >> 16;
