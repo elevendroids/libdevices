ed542d324640d4e298a207ceec3fd3be88be380e
Michal Potrzebicz <michal@elevendroids.com>
2013-05-29 00:31:41 +0200

    [platform/msp430] Timer-based delay_ms - switches to LPM1 while waiting

--- platform/msp430/delay.c
+++ platform/msp430/delay.c
@@ -1,4 +1,8 @@
 #include <msp430.h>
+#include <stdint.h>
+
+static int ticks_per_ms = 1000;
+static volatile uint16_t timer_ticks = 0;
 
 inline void delay_us(unsigned int useconds)
 {
@@ -7,11 +11,24 @@ inline void delay_us(unsigned int useconds)
 	} while (--useconds);
 }
 
-inline void delay_ms(unsigned int mseconds)
+void delay_ms(unsigned int mseconds)
 {
-	while(mseconds--) {
-		__delay_cycles(1000 * 8);
-	}
+	timer_ticks = mseconds;
+	TA0CCR0  = ticks_per_ms;
+	TA0CCTL0 = CCIE;
+	TA0CTL  = TASSEL_2 | MC_1 | ID_3;
+	do {
+		LPM1;
+	} while (timer_ticks);
 }
 
-
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void OnTickTimer(void)
+{
+	if (timer_ticks) {
+		timer_ticks--;
+	} else {
+		TA0CTL &= ~MC_3;
+		LPM1_EXIT;
+	}
+} 
