8bbca58bc69ffe408a68308788a2abc1a19f5111
Michal Potrzebicz <michal@elevendroids.com>
2013-06-02 00:54:50 +0200

    [sensor/bmp085] Header cleanup

--- include/sensor/bmp085.h
+++ include/sensor/bmp085.h
@@ -18,17 +18,16 @@
 #define BMP085_REG_MC	0xBC
 #define BMP085_REG_MD	0xBE
 
-
-
 #define BMP085_SAMPLES_1 0
 #define BMP085_SAMPLES_2 1
 #define BMP085_SAMPLES_4 2
 #define BMP085_SAMPLES_8 3
 
-extern int Bmp085_Init(I2cDevice *device);
-extern int32_t Bmp085_ReadPressure(I2cDevice *device, uint8_t oversampling);
-extern int Bmp085_ReadTemperature(I2cDevice *device);
+int Bmp085_Init(I2cDevice *device);
+int32_t Bmp085_ReadPressure(I2cDevice *device, uint8_t oversampling);
+int Bmp085_ReadTemperature(I2cDevice *device);
 int Bmp085_ReadChipId(I2cDevice *device);
 int Bmp085_ReadChipVersion(I2cDevice *device);
 
 #endif
+
