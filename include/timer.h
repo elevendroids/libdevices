756fa9fa9e1ab990b3137dc3e1c65c2b1abf3d8b
Michal Potrzebicz <michal@elevendroids.com>
2014-01-29 22:19:47 +0100

    [MSP430] Reworked tick timer code, support for XT1 32kHz crystal and VLO auto-calibrated ACLK

--- include/timer.h
+++ include/timer.h
@@ -1,29 +1,69 @@
 /*
-*
-* Copyright (c) 2013, Michal Potrzebicz <michal@elevendroids.com>
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following 
-* conditions are met:
-*
-*    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
-*      disclaimer.
-*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
-*      disclaimer in the documentation and/or other materials provided with the distribution.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-*  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-*  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-*  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-*  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-*/
+ *
+ * @file timer.h
+ * @author Michal Potrzebicz
+ * @date
+ *
+ * @brief Simple tick timer
+ *
+ * @section LICENSE
+ *
+ * Copyright (c) 2013, Michal Potrzebicz <michal@elevendroids.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following 
+ * conditions are met:
+ *
+ *    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ *      disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 #ifndef TIMER_H
 #define TIMER_H
 
-void Timer_SleepMSec(unsigned interval);
-void Timer_SleepSec(unsigned interval);
+#include <stdbool.h>
+
+/**
+ * @brief Initialize timer
+ */
+void Timer_Init(void);
+
+/**
+ * @brief Get current tick (ms) count
+ * @returns Number of ticks
+ */
+unsigned int Timer_GetTicks(void);
+
+/**
+ * @brief Calculate difference between tick values
+ * @param start - initial ticks value
+ * @param end - final ticks value
+ * @returns Number of elapsed ticks between start and end
+ */
+unsigned int Timer_ElapsedTicks(unsigned int start, unsigned int end);
+
+/**
+ * @brief Check if a timeout occured between current and saved tick value
+ * @param ticks - last ticks value
+ * @param timeout - timeout value
+ * @returns true if timeout has occured
+ */
+bool Timer_IsTimeOut(unsigned int ticks, unsigned int timeout);
+
+/**
+ * @brief Sleeps (board specific, using PM if available) for a given number of miliseconds
+ * @param ticks - number of ticks (ms) to sleep
+ */
+void Timer_Sleep(unsigned int ticks);
 
 #endif
--- src/platform/msp430/timer.c
+++ src/platform/msp430/timer.c
@@ -20,92 +20,143 @@
 *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *
 */
+
 #include <msp430.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <limits.h>
 #include "platform/msp430.h"
 
-static volatile unsigned intervalCounter = 0;
+volatile static unsigned int miliseconds;
+volatile static unsigned int sleep_ms;
 
-void Timer_SleepMSec(unsigned interval)
+//TODO: Make it board-specific
+#define USE_EXTERNAL_CRYSTAL
+
+#ifdef USE_EXTERNAL_CRYSTAL
+static const uint16_t us_per_tick = 1953;
+
+void Timer_Init(void) 
 {
-	intervalCounter = interval;
-	// set timer period
-	TA0CCR0 = Msp430_cyclesPerMs - 1;
-	// reset timer
-	TA0R = 0x00;
-	// clock source = SMCLK, up mode, TAIV interrupt enabled
-	TA0CTL = TASSEL_2 | MC_1 | TAIE;
-	do {
-		// sleep with SMCLK running
-		_BIS_SR(LPM1_bits | GIE);
-	} while (intervalCounter);
-	// stop timer, disable interrupt
-	TA0CTL &= ~(MC_1 | TAIE);
+	BCSCTL3 = LFXT1S_0 | XCAP_3;
+	WDTCTL = WDT_ADLY_1_9;
+	IE1 |= WDTIE;
+}
+#else
+static uint16_t us_per_tick;
+
+static uint16_t vlo_us_per_tick(void) {
+	uint16_t cycles;
+	
+	BCSCTL1 = CALBC1_1MHZ;
+	DCOCTL = CALDCO_1MHZ;
+
+	TA0CTL = TASSEL_2 | ID_0 | TACLR;
+	TA0CCTL0 = CM_1 | CCIS_1 | CAP | SCS;
+	TA0CTL |= MC_2;
+
+	while ((TA0CCTL0 & CCIFG) == 0);
+	TA0CCR0 = 0;
+	TA0CCTL0 &= ~CCIFG;
+
+	while ((TA0CCTL0 & CCIFG) == 0);
+	cycles = TA0CCR0;
+	TA0CCTL0 &= ~CCIFG;
+
+	while ((TA0CCTL0 & CCIFG) == 0);
+	cycles = TA0CCR0 - cycles;
+	TA0CCTL0 &= ~CCIFG;
+
+	TA0CTL = TACLR;
+
+	Msp430_InitClock();
+
+	return (cycles * 64);
 }
 
-uint16_t Timer_GetVLOFrequency(void)
+void Timer_Init(void) 
 {
-	uint16_t first_capture, ticks_per_vlo;
-	uint8_t saved_clock;	
-	// save old clock setting
-	saved_clock = Msp430_currentClock;
+	BCSCTL3 = LFXT1S_2;
+	us_per_tick = vlo_us_per_tick();
+	WDTCTL = WDT_ADLY_1_9;
+	IE1 |= WDTIE;
+}
+#endif
 
-	// switch to 1MHz clock
-	Msp430_SetClock(MSP430_CLOCK_1MHZ);
-	// reset timer
-	TA0R = 0x00;
-	// set TimerA in continous mode, set capture mode triggered by ACLK
-	TA0CCTL0 = CM_1 | CCIS_1 | CAP;
-	TA0CTL = TASSEL_2 | ID_0 | MC_2;
-	// skip capture
-	while (! (TACCTL0 & CCIFG));
-	TACCTL0 &= ~CCIFG;
-	// get first capture
-	while (! (TACCTL0 & CCIFG));
-	first_capture = TACCR0;
-	TACCTL0 &= ~CCIFG;
-	// get second capture
-	while (! (TACCTL0 & CCIFG));
-	ticks_per_vlo = TACCR0 - first_capture;
-	// disable timer
-	TA0CTL &= ~MC_1;	
-	TA0CCTL0 = 0x00;
-	// restore clock
-	Msp430_SetClock(saved_clock);
-	// return VLO frequency (in Hz)
-	return (1000000 / ticks_per_vlo);
+unsigned int Timer_GetTicks(void) 
+{
+	return miliseconds;
 }
 
-void Timer_SleepSec(unsigned interval)
-{
-	intervalCounter = interval;
-	// set timer period to ACLK frequency - one interrupt per secnd
-	TA0CCR0 = Timer_GetVLOFrequency() - 1;
-	// reset timer
-	TA0R = 0x00;
-	// clock source = ACLK, up mode, TAIV interrupt enable
-	TA0CTL = TASSEL_1 | MC_1 | TAIE;
-	do {
-		// deep sleep mode
-		_BIS_SR(LPM3_bits | GIE);
-	} while (intervalCounter);
-	// stop timer and disable interrupt
-	TA0CTL &= ~(MC_1 | TAIE);
+unsigned int Timer_ElapsedTicks(unsigned int start, unsigned int end) {
+	if (start > end)
+		return end + (UINT_MAX - start);
+	else
+		return end - start;
 }
 
-#pragma vector=TIMER0_A1_VECTOR
-__interrupt void OnTickTimer(void)
-{
-	#ifdef __MSP430_HAS_TA3__
-	if (TA0IV == TA0IV_TAIFG) {
-	#endif
-	#ifdef __MSP430_HAS_TA2__
-	if (TAIV == TAIV_TAIFG) {
-	#endif
-		intervalCounter--;
-		if (intervalCounter == 0) {
-			_BIC_SR_IRQ(LPM3_bits); // wake from LPM0-3
-		}
+bool Timer_IsTimeOut(unsigned int ticks, unsigned int timeout) {
+	unsigned int current_ticks = Timer_GetTicks();
+	unsigned int elapsed_ticks;
+
+	elapsed_ticks = Timer_ElapsedTicks(ticks, current_ticks);
+
+	return (elapsed_ticks >= timeout);
+}
+
+void Timer_Sleep(unsigned int ticks) {
+	unsigned int interval;
+	uint8_t ms_per_tick;
+	
+	ms_per_tick = (us_per_tick / 1000);
+
+	if (ticks <= ms_per_tick)
+		interval = ms_per_tick + 1;
+	else
+		interval = ticks;
+
+	_DINT();
+	sleep_ms = interval;
+	_bis_SR_register(LPM3_bits | GIE);
+
+/*
+	while (sleep_ms) {
+		_DINT();
+		if (sleep_ms)
+			_bis_SR_register(LPM3_bits | GIE);
+		else
+			_EINT();
 	}
-} 
+*/
+}
+
+#pragma vector=WDT_VECTOR
+__interrupt void Watchdog_ISR(void)
+{
+	static unsigned int microseconds = 0;
+	unsigned int sleep_counter;
+	unsigned int elapsed_ms = 0;
+	
+	microseconds += us_per_tick;
+	while (microseconds >= 1000 ) {
+		elapsed_ms++;
+		microseconds -= 1000;
+	}
+
+	sleep_counter = sleep_ms;
+	if (sleep_counter) {
+		if (sleep_counter >= elapsed_ms)
+			sleep_counter -= elapsed_ms;
+		else
+			sleep_counter = 0;
+		
+		if (sleep_counter == 0)
+			_bic_SR_register_on_exit(LPM3_bits);
+		
+		sleep_ms = sleep_counter;
+	}
+
+	miliseconds += elapsed_ms;
+}
+
 
